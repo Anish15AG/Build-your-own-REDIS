be6773198cb6bfc37bf0b3ac84fb71c15d97db3c
Anish <anish.agclg@gmail.com>
2024-10-17 00:36:04 +0530

    Added SET and GET functions

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
 const net = require('net');
 const Parser = require('redis-parser');
 
-const server = net.createServer(connection=> {
-    console.log('Client Connected...')
+const store = {};
 
-    connection.on('data', data => {
+const server = net.createServer((connection) => {
+    console.log('Client Connected...');
+
+    connection.on('data', (data) => {
         const parser = new Parser({
-            returnReply: (reply)=>{
-                console.log('=>', reply);
+            returnReply: (reply) => {
+                const command = reply[0].toLowerCase(); // Normalize command to lowercase
+                switch (command) {
+                    case 'set': {
+                        const key = reply[1];
+                        const value = reply[2];
+                        store[key] = value;
+                        connection.write('+OK\r\n'); // Acknowledge successful set
+                        break;
+                    }
+                    case 'get': {
+                        const key = reply[1];
+                        const value = store[key];
+                        if (value === undefined) {
+                            connection.write('$-1\r\n'); // Null Bulk String for non-existent key
+                        } else {
+                            // Use a Bulk String response, but it will still just send the value.
+                            connection.write(`$${value.length}\r\n${value}\r\n`);
+                        }
+                        break;
+                    }
+                    default: {
+                        connection.write('-ERR unknown command\r\n'); // Handle unknown commands
+                    }
+                }
             },
-            returnError: (err) =>{
-                console.log('=>', err);
+            returnError: (err) => {
+                console.error('Parser Error:', err);
+                connection.write(`-ERR ${err.message}\r\n`);
             }
-        })
-        parser.execute(data);
-        connection.write('+OK\r\n')
-    })
+        });
 
-})
+        parser.execute(data); // Process incoming data through the Redis parser
+    });
 
-server.listen(8000, () => console.log("Custom REDIS server running on Port 8000")) //Default server for REDIS2.0 is 8000 ideal is 6379
+});
+
+server.listen(8000, () => console.log("Custom REDIS server running on Port 8000"));
