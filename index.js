86aad37a60be7c12da2dc4d6ce3f09c151f47bb6
Anish <anish.agclg@gmail.com>
2024-10-17 00:11:24 +0530

    Setup Parser

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const net = require('net');
+const Parser = require('redis-parser');
 
 const server = net.createServer(connection=> {
     console.log('Client Connected...')
+
+    connection.on('data', data => {
+        const parser = new Parser({
+            returnReply: (reply)=>{
+                console.log('=>', reply);
+            },
+            returnError: (err) =>{
+                console.log('=>', err);
+            }
+        })
+        parser.execute(data);
+        connection.write('+OK\r\n')
+    })
 
 })
 
